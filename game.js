380a6206301e95b59235266c9aface13934ab188
dbustanUCSC <dbustan@ucsc.edu>
2023-05-15 13:16:08 -0700

    Map is now bigger, with the cost of it repeating.

--- game.js
+++ game.js
@@ -39,33 +39,45 @@ class Level1 extends Phaser.Scene {
     preload(){
         this.load.image('Scene 1', './Scene 1.png');
         this.load.image('Dandy', './Main Character.png');
+        this.load.image('coin', './Coint.png');
     }
     create(){
         this.cursors = this.input.keyboard.createCursorKeys();
         this.cameras.main.fadeIn(1000);
-        
+        this.cameras.main.setBounds(0, 0, 2560 * 2, 1440 * 2);
+        this.physics.world.setBounds(0, 0, 2560 * 2, 1440 * 2);
         // Create the background image centered on the screen
         let Background = this.add.image(0, 0, 'Scene 1');
         Background.setPosition(this.cameras.main.centerX, this.cameras.main.centerY);
-
+        this.add.image(0, 0, 'Scene 1').setOrigin(0);
+        this.add.image(2560, 0, 'Scene 1').setOrigin(0).setFlipX(true);
+        this.add.image(0, 1440, 'Scene 1').setOrigin(0).setFlipY(true);
+        this.add.image(2560, 1440, 'Scene 1').setOrigin(0).setFlipX(true).setFlipY(true);
         // Create the Dandy sprite centered on the screen
         this.Dandy = this.physics.add.sprite(0, 0, 'Dandy');
         this.Dandy.setScale(0.3);
         this.Dandy.setPosition(this.cameras.main.centerX, this.cameras.main.centerY);
 
         // Set the world bounds and enable collision with the world bounds
-        this.physics.world.setBounds(0, 0, this.cameras.main.centerX+2560/2,  this.cameras.main.centerY + 1440/2);
         this.Dandy.setCollideWorldBounds(true);
         this.cameras.main.setZoom(4);
-        this.cameras.main.setBounds(0, 0, 2560, 1440);
+        //this.cameras.main.setBounds(0, 0, 2560, 1440);
         
         // Start the camera following the Dandy sprite, centered on the screen
         this.cameras.main.startFollow(this.Dandy, true, 0.05, 0.05, 0, 0, 2560, 1440);
         
+        for (let i = 0; i < 20; i++) {
+            let x = Phaser.Math.RND.between(0, this.physics.world.bounds.width);
+            let y = Phaser.Math.RND.between(0, this.physics.world.bounds.height);
+            let element = this.add.image(x, y, 'coin');
+            element.setScale(0.2);
+            // Set the origin to the center of the element for better positioning
+            element.setOrigin(0.5);
+        }
     }
     update(){
         this.Dandy.setMaxVelocity(200);
-        this.Dandy.setDrag(700);
+        this.Dandy.setDrag(500);
         if (this.cursors.left.isDown)
         {
             this.Dandy.setAccelerationX(-100);
@@ -91,10 +103,6 @@ class Level1 extends Phaser.Scene {
     }
 }
 new Phaser.Game({
-    width: 2560,
-    height: 1440,
-    scene: [Intro],
-
     loader: {
         baseURL: 'assets/'
     },
@@ -108,9 +116,8 @@ new Phaser.Game({
 
     scale: {
         mode: Phaser.Scale.FIT,
-        autoCenter: Phaser.Scale.CENTER_BOTH.CENTER_BOTH,
-        width: 2560,
-        height: 1440
+        width: 2560 * 2,
+        height: 1440 * 2
     },
     scene: [Intro, Level1],
     title: "Dandy Game",
